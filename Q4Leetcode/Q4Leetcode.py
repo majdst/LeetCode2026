1a5c65402df07341ff929ca4698ac0ef1404a070
Majid Dousti <dousti.explore@gmail.com>
2026-01-19 17:27:09 -0500

    Using python built in function

--- Q4Leetcode/Q4Leetcode.py
+++ Q4Leetcode/Q4Leetcode.py
@@ -2,26 +2,19 @@ class Solution:
 
     def twolist(self, list1: list, list2: list)->int:
 
-        """Input: nums1 = [1,3], nums2 = [2]"""
+        merge_list = list1+list2
 
-        list_new = list1 + list2
+        merge_list.sort()
 
-        m = len(list_new)
-        
-        swap = True
-        while swap: # sort the new list
-            swap = False
-            for num in range(m-1):
 
-                if list_new[num] > list_new[num+1]:
+        m = len(merge_list)
 
-                    list_new[num], list_new[num+1] = list_new[num+1], list_new[num] 
-                    swap = True
-        
-        if m % 2 == 0:
-            median = (list_new[m//2]+list_new[(m//2)-1])/2
+        if m%2 == 0:
+
+            median = (merge_list[m//2] + merge_list[m//2 - 1])/2
+
         else:
-            median = list_new[m//2]
+            median = merge_list[m//2]
 
         return median
     
