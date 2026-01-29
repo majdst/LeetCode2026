d46314ba9336a43c647c2022c8eea0fa65424ff1
Majid Dousti <dousti.explore@gmail.com>
2026-01-29 14:09:17 -0500

    More efficient: very similar to linkedlist question

--- Q4Leetcode/Q4Leetcode.py
+++ Q4Leetcode/Q4Leetcode.py
@@ -6,25 +6,44 @@ Explanation: merged array = [1,2,3] and median is 2."""
 
 class Solution:
     def median(self, num1:list, num2:list)->int:
+        newlist = []
+        right = 0
+        left = 0
 
-        x = num1 + num2
+        while right < len(num1) and left < len(num2):
 
-        x.sort()
+            if num1[right] < num2[left]:
 
-        m = len(x)
-        median = 0
+                newlist.append(num1[right])
+                right += 1
+            else:
+                newlist.append(num2[left])
+                left += 1
 
-        if m % 2 == 0:
+        while right < len(num1):
+            newlist.append(num1[right])
+            right += 1
 
-            median = (x[m//2 - 1] + x[m//2]) * 0.5
-        
+        while left < len(num2):
+            newlist.append(num2[left])
+            left += 1
+
+        m = len(newlist)
+        if m %2 == 0:
+
+            median = (newlist[m//2 - 1] + newlist[m//2])*0.5
+
         else:
-
-            median = x[m//2]
-
-
+            median = (newlist[m//2])
+        
         return median
+    
+
+
+            
+           
+           
 
 y = Solution()
-print(y.median([1,3], [2, 4]))
+print(y.median([1,3], [2, 4, 5]))
 
