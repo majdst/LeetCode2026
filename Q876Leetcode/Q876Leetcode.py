ebefde519248e2e75ac39ed4fb506399882b07b6
Majid Dousti <dousti.explore@gmail.com>
2026-01-30 17:38:53 -0500

    Question 876 Leetcode

---
+++ Q876Leetcode/Q876Leetcode.py
@@ -0,0 +1,60 @@
+class Node:
+
+    def __init__(self, val:int, next = None):
+
+        self.next = next
+        self.val = val
+
+class LinkedList:
+    def helper(self, l:list)->Node:
+
+        dummy = Node(0)
+        current = dummy
+
+        for i in l:
+            newnode = Node(i)
+
+            current.next = newnode
+            current = current.next
+
+        return dummy.next
+    
+    def pr(self, l:Node):
+
+        current = l
+
+        while current:
+            print(current.val, end = " -> ")
+            current = current.next
+        print("None")
+
+    def middle(self, l:Node):
+
+        def length(lnode:Node):
+
+            current = lnode
+
+            m = 0
+
+            while current:
+                current = current.next
+                m += 1
+            return m
+        
+        lng = length(l)
+
+        current = l
+        m = 0
+
+        
+
+        while m < (lng//2):
+            current = current.next
+            m += 1
+
+        return current
+    
+x = LinkedList()
+x1 = x.helper([1,2,3,4,5,6])
+x.pr(x1)
+x.pr(x.middle(x1))
