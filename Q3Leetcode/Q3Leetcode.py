bc07a87cf7ddc35ae40f3473fa99f87bbc61bf69
Majid Dousti <dousti.explore@gmail.com>
2026-03-21 14:06:21 -0400

    Adding both methods with explanation of Time Complexity is added

--- Q3Leetcode/Q3Leetcode.py
+++ Q3Leetcode/Q3Leetcode.py
@@ -1,22 +1,43 @@
 class Solution:
     def leng(self, s:str)->int:
-
-        l = len(s)
-        seen = set()
-        left = 0
-        max_l = 0
         
-        for right in range(l):
-            if s[right] in seen:
+        #Brutforce method with TimeComplexity of O(n^3)
+        l = len(s)
+        max_l = 0
 
-                seen.remove(s[left])
-                left += 1
-
-            seen.add(s[right])
-            max_l = max(max_l, right - left + 1)
-
+        for i in range(l): # O(n)
+            for j in range(i+1, l+1): #O(n)
+                substring = s[i:j] #O(n)
+            
+                if len(substring) == len(set(substring)):
+                    max_l = max(max_l, len(substring))
         
         return max_l
     
+    def longlen(self, s:str)->int:
+
+        #SET method --> Time complexity O(n)
+        l = len(s)
+        max_l = 0
+        left = 0
+        n = set()
+
+        for right in range(l):
+
+            while s[right] in n:
+                n.remove(s[left])
+                left += 1
+            
+            n.add(s[right])
+
+            max_l = max(max_l, right - left + 1)
+        
+        return max_l
+    
+
+
+
+
+
 mj = Solution()
-print(mj.leng("abcdabcbb"))
+print(mj.longlen("abcabcbb"))
