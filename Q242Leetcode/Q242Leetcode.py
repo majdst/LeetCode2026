fe5aa33527ae2f79895364f77cf9527bbd355463
Majid Dousti <dousti.explore@gmail.com>
2026-03-31 17:39:07 -0400

    One line solution: Pro one

--- Q242Leetcode/Q242Leetcode.py
+++ Q242Leetcode/Q242Leetcode.py
@@ -1,21 +1,9 @@
 class solution:
 
     def isAnagram(self, s: str, t: str) -> bool:
-        #Not a efficient method. 
-        newD = {}
-        newL = []
-        newL.append(s)
-        newL.append(t)
 
-        for item in newL:
-            key = "".join(sorted(item))
-
-            if key not in newD:
-                newD[key] = [item]
-            else:
-                newD[key].append(item)
-            
-        return len(newD) == 1
+        return sorted(s) == sorted(t)
+    
     
 x = solution()
-print(x.isAnagram("anagram", "nagaram"))  
+print(x.isAnagram("sfgram", "nagaram"))  
