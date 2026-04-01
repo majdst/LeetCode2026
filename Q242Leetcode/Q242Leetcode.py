fce6ea5dfb89f417d5e18051eac82deb5442a246
Majid Dousti <dousti.explore@gmail.com>
2026-04-01 15:30:38 -0400

    Long one but cool one

--- Q242Leetcode/Q242Leetcode.py
+++ Q242Leetcode/Q242Leetcode.py
@@ -4,6 +4,24 @@ class solution:
 
         return sorted(s) == sorted(t)
     
+    def isAnagram1(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+        
+        newD = {}
+
+        for char in s:
+            newD[char] = newD.get(char, 0) + 1
+
+        for char in t:
+            newD[char] = newD.get(char, 0) - 1
+
+            if newD[char] < -1:
+                return False
+        
+        return True
+    
     
 x = solution()
-print(x.isAnagram("sfgram", "nagaram"))  
+print(x.isAnagram1("a", "ab"))  
