606342b032e5cafdb447b2b520e0a275c1f3a13b
Majid Dousti <dousti.explore@gmail.com>
2026-04-01 20:57:54 -0400

    Adding the efficient methods

--- Q643Leetcode/Q643.py
+++ Q643Leetcode/Q643.py
@@ -1,15 +1,30 @@
 class Solution:
     def findMaxAverage(self, nums: list, k: int) -> float:
-
-        maxSum = float("-inf")
+        #Bruteforce O(n^2) --> For big lists, it's not working
+        maxS = float('-inf')
         l = len(nums)
         for i in range(l-k+1):
             trg = 0
             for j in range(i, i+k):
                 trg += nums[j]
-            
-            if trg > maxSum:
-                maxSum = trg
-        return maxSum/k
+
+            if trg > maxS:
+                maxS = trg
+        return maxS/k
+    
+    def findMaxAverage1(self, nums: list, k: int) -> float:
+        #O(n) Time Complexity 
+        newS = sum(nums[:k])
+        maxS = newS
+        l = len(nums)
+
+        for i in range(k, l):
+            newS += nums[i] - nums[i-k]
+
+            if newS > maxS:
+                maxS = newS
+        return maxS/k
+
+     
 x = Solution()
-print(x.findMaxAverage([1,12,-5,-6,50,3], 4))
+print(x.findMaxAverage1([5], 1))
