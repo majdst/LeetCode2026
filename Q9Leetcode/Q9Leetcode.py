2bb369e2015e05460a336354ea986daddd48cb0f
Majid Dousti <dousti.explore@gmail.com>
2026-04-04 12:37:21 -0400

    New Method is added

--- Q9Leetcode/Q9Leetcode.py
+++ Q9Leetcode/Q9Leetcode.py
@@ -2,7 +2,24 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
 
        return str(x) == str(x)[::-1]
+    
+    def isPalindrome1(self, x: int) -> bool:
+
+        if x < 0:
+            return False
+        remain, numb = 0, 0
+        orig = x
+
+        while x:
+            remain = x % 10
+
+            numb = numb * 10 + remain
+
+            x //= 10
+        
+        return orig == numb
+        
        
 
 m = Solution()
-print(m.isPalindrome(-121))
+print(m.isPalindrome1(121))
