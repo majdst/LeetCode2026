34985b0cb0d05470d702774454c810228c881ed8
Majid Dousti <dousti.explore@gmail.com>
2026-04-06 09:32:10 -0400

    Better solution

--- Q29Leetcode/Q29Leetcode.py
+++ Q29Leetcode/Q29Leetcode.py
@@ -17,5 +17,28 @@ class Solution:
         return min(max(count, -2**31), 2**31-1)
     # this one line is instead of if, elif, and else
 
+    def divide1(self, dividend: int, divisor: int) -> int:
+
+        neg = (dividend < 0) != (divisor < 0)
+        #Handling the negative
+
+        D = abs(dividend)
+        d = abs(divisor)
+        count = 0
+
+        while D >= d:
+            temp = d
+            multi = 1
+            while D >= (temp << 1):
+                temp <<= 1
+                multi <<= 1
+            
+            D -= temp
+            count += multi
+        
+        if neg:
+            count = -count
+
+        return min(max(count, -2**31), 2**31 - 1)
 x = Solution()
-print(x.divide(25, -2))
+print(x.divide1(25, 2))
---
+++ Q29Leetcode/README.md
@@ -0,0 +1,6 @@
+For this Question, there are two different method: brutforce which is simple but inefficient and
+
+a new method. Just imagine, in this question, I learned how to manage negative numbers, how to deal with multiplying without using multiple.
+
+!\[New way to deal with if condition](01.jpg)
+
