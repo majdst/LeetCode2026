020d87209d97cfd527b5650e118386230b4d3843
Majid Dousti <dousti.explore@gmail.com>
2026-04-23 16:44:38 -0400

    Better innovative solution

--- Q66Leetcode/Q66.py
+++ Q66Leetcode/Q66.py
@@ -14,6 +14,16 @@ class Solution:
         
         newL.reverse()  # digits were added backwards
         return newL
+    
+    def plusOne1(self, digits: list):
+        l = len(digits)
+
+        for i in range(l-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[0]
+        return [1] + digits
 
 x = Solution()
-print(x.plusOne([4,3,2,1]))
+print(x.plusOne1([4,3,2,1]))
