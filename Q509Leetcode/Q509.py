e3b7d34151a5efce05ea70d36156d62b6e9ffdd1
Majid Dousti <dousti.explore@gmail.com>
2026-04-26 00:30:28 -0400

    Dynamic programming

--- Q509Leetcode/Q509.py
+++ Q509Leetcode/Q509.py
@@ -1,6 +1,6 @@
 class Solution:
     def fibo(self, n:int)->int:
-
+        #O(2^n)
         def helper(x:int):
             if x <= 1:
                 return x
@@ -9,5 +9,17 @@ class Solution:
         
         return helper(n)
     
+    def fibo1(self, n:int)->int:
+        def helper(x):
+            newD = {}
+            if x <= 1:
+                return x
+            if x in newD:
+                return newD[x]
+            
+            newD[x] = helper(x-1) + helper(x-2)
+            return newD[x]
+        
+        return helper(n)
 x = Solution()
-print(x.fibo(20))
+print(x.fibo1(20))
